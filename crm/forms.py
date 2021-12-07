d874ff70710d850856e41a72520952403b471b91
SergeyL-1979 <testovuj1979@gmail.com>
2021-12-07 20:09:15 +0300

    Изменил название проекта config

---
+++ crm/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from django.forms import Form
+
+
+class OrderForm(forms.Form):
+    name = forms.CharField(max_length=200, widget=forms.TextInput(attrs={'class': 'css_input'}))
+    phone = forms.CharField(max_length=200, widget=forms.TextInput(attrs={'class': 'css_input'}))
+
+
+
